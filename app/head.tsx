71ee76e89947b05973fd6e453e621ab6fb4b3da2
hellohasnain <hasnainraza1059@gmail.com>
2025-04-18 16:09:43 +0530

    head

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,20 @@ function Head() {
     <>
       <title>CG Inside</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/images/favicon.ico" />
+      <link rel="icon" href="/images/.ico" />
+
+      {/* Google tag (gtag.js) */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-YJM794YGV2"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YJM794YGV2');
+          `,
+        }}
+      />
     </>
   );
 }
